eaa8025ba8a5fbb2af75311875fca1f421bfd0b2
Andrea Huang <huangandrea@hotmail.it>
2025-04-02 22:19:19 +0200

    4. React Essentials :: lifting state up to nearest common ancestor

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initalGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({}) {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initalGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
       const updatedGameBoard = [...prevGameBoard.map((row) => [...row])];
-      updatedGameBoard[rowIndex][colIndex] = "X";
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedGameBoard;
     });
+
+    onSelectSquare();
   }
 
   return (
