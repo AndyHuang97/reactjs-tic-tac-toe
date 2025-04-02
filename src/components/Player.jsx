2a6fa149e86edb313b56d5835c7c211ae75db9a0
Andrea Huang <huangandrea@hotmail.it>
2025-04-02 16:45:42 +0200

    4. React Essentials :: add edit and save with suboptimal state update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,28 @@
-export default function player({name, symbol}) {
+import { useState } from "react";
+
+export default function player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) playerName = <input type="text" required value={name} />;
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <span>
-        <button>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span>
     </li>
   );
