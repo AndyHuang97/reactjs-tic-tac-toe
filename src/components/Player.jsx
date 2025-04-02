6447414bee8cce23780c8e7e1c61353e4ac03559
Andrea Huang <huangandrea@hotmail.it>
2025-04-02 14:48:28 +0200

    4. React Essentials :: concept repeition -> splitting and reusing components

---
+++ src/components/Player.jsx
@@ -0,0 +1,13 @@
+export default function player({name, symbol}) {
+  return (
+    <li>
+      <span className="player">
+        <span className="player-name">{name}</span>
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <span>
+        <button>Edit</button>
+      </span>
+    </li>
+  );
+}
